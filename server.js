f7d06efa25a628c53d79e4efea677ef52645051b
Emi <emi_chang@lhwlfw.com>
2022-04-26 12:21:47 +0800

    add /rooms router

--- server.js
+++ server.js
@@ -37,10 +37,23 @@ Room.create({
 }).then(res => console.log(res, "新增成功")
 ).catch(err => console.log(err));
 
-(async () => await Room.find())().then(res => console.log(res));
-
-const requestListener = async (req, res) => {
-    res.end();
+const requestListener = async (req,res)=>{
+    const headers = {
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization, Content-Length, X-Requested-With',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+        'Content-Type': 'application/json'
+    }
+    if(req.url=="/rooms" && req.method=="GET"){
+        const rooms = await Room.find();
+        res.writeHead(200,headers);
+        res.write(JSON.stringify({
+            "status":"success",
+            rooms
+        }))
+        res.end()
+    }
 }
+
 const server = http.createServer(requestListener);
 server.listen(3000);
